4fd22710f0318d329dc60532fd0a1f0dbee5dce4
Thirawat Akkasriwon <thirawat.dev@gmail.com>
2025-07-05 10:15:40 +0700

    update CustomUserDetailsService

--- src/main/java/com/example/taskmanagement/service/CustomUserDetailsService.java
+++ src/main/java/com/example/taskmanagement/service/CustomUserDetailsService.java
@@ -2,6 +2,8 @@ package com.example.taskmanagement.service;
 
 import com.example.taskmanagement.entity.User;
 import com.example.taskmanagement.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,13 +16,19 @@ import java.util.List;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
+
     @Autowired
     private UserRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        logger.info("#### Custom User Detail Service Active ####");
         User user = repository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+                .orElseThrow(() -> {
+                    logger.warn("User not found with email: {}", email);
+                    return new UsernameNotFoundException("User not found with email: " + email);
+                });
 
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
